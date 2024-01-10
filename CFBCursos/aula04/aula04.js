1b985a4dd3bfbd20e8d8ce999cb6e3a5119c43e2
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-10 18:41:47 -0300

    aula04 criada

---
+++ CFBCursos/aula04/aula04.js
@@ -0,0 +1,25 @@
+let num1 = 5, num2 = 10, //Declaração das variáveis
+
+soma = num1 + num2 //Soma
+console.log(soma) //Impressão
+
+mult = soma*2 //Multiplicação
+console.log(mult) //Impressão
+
+div = mult / soma //Divisão
+console.log(div) //Impressão
+
+rest = mult % soma //Resto da divisão
+console.log(rest) //Impressão
+
+num1++ //Encrementa de 1 em 1
+console.log(num1) //Impressão
+
+num2-- //Dencrementa de 1 em 1
+console.log(num2) //Impressão
+
+num1 += 5 //Soma 5 na variável num1(num1 = num1 + 5)
+console.log(num1) //Impressão
+
+num2 -= 5 //Subtrai 5 na variável num1(num1 = num1 - 5)
+console.log(num2) //Impressão
