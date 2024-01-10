fa035601d44af1dd87d35977f40a0184b0211dc1
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-10 19:16:21 -0300

    aula007 criada

---
+++ CFBCursos/aula007/aula007.js
@@ -0,0 +1,10 @@
+let n1 = 10, n2 = 5, n3 = 15, n4 = 2 //Declaração de variáveis
+
+E = n1 > n2 && n1 > n3 //&& = e
+console.log(E) //Impressão
+
+Ou = n1 > n2 || n1 > n3 //|| = ou
+console.log(Ou) //Impressão
+
+not = !(n1 > n2) //! = não
+console.log(not) //Impressão
