d61c579661f89710d8732badcf9e7ebc2e521069
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-10 18:54:10 -0300

    aula05 criada

---
+++ CFBCursos/aula05/aula05.js
@@ -0,0 +1,19 @@
+let num1 = 10, num2 = 5, num3 = 10 //Declaração de variáveis
+
+maior = num1 > num2 // Maior do que ...
+console.log(maior) // Impressão
+
+menor = num1 < num2 // Menorr do que ...
+console.log(menor) // Impressão
+
+maiorIgual = num1 >= num3 // Maior ou igual que ...
+console.log(maiorIgual) // Impressão
+
+menorIgual = num2 <= num3 // Menor ou igaul que ...
+console.log(menorIgual) // Impressão
+
+Igual = num1 == num3 // Igual a ...
+console.log(Igual) // Impressão
+
+Diferente = num1 != num3 //Diferente de ...
+console.log(Diferente) // Impressão
