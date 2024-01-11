8da83c2eba17fec3f18af46a28b9d27fa8063071
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-11 18:58:19 -0300

    aula009 criada

---
+++ CFBCursos/aula009/aula009.js
@@ -0,0 +1,10 @@
+let n1 = n2 = n3 =10 //Declaração de variáveis
+
+n1++ //Incrementação
+
+console.log(n1) //Impressão
+
+console.log(++n2) //Impressão de pré-incremento
+
+console.log(n3++) //Impressão de pos-incremento
+console.log(n3) //Impressão
