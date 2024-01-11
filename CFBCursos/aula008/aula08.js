14e462034036f4f19b5ee093dfe3106e96a1fb1b
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-11 18:42:20 -0300

    aula08 criada

---
+++ CFBCursos/aula008/aula08.js
@@ -0,0 +1,7 @@
+let n1 = 10, n2 = 11 //Declaração de variáveis
+
+let e = n1 || n2 // n1 e n2
+console.log(e) //Impressão em  bitwise
+
+let ou = n1 && n2 // n1 ou n2
+console.log(ou) //Impressão em  bitwise
