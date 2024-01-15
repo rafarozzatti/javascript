961e7ae6ac05f2f724e065288400afad8a5cf3a5
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-15 18:29:27 -0300

    aula015 criada

---
+++ CFBCursos/aula015/aula015.js
@@ -0,0 +1,10 @@
+console.log("Inicio do programa")
+
+for (let i = 1; i <= 100; i++) {
+    if(i%2==0) {
+        console.log(i + " é par")
+    } else {
+        console.log(i + " é impar")
+    } 
+}
+console.log("Fim do programa")
