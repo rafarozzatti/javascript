b80cb9b7bab228c9afe9d1b0a368a8af3f448547
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-15 17:55:49 -0300

    aula013 criada

---
+++ CFBCursos/aula013/aula013.js
@@ -0,0 +1,10 @@
+let num = 10
+
+if (num > 10) {
+    console.log("Numeral maior que 10")
+} else if (num > 5){
+    console.log("Numeral esta entre 6 e 10")
+} else {
+    console.log("Numeral menor ou igual que 10")
+}
+console.log("Fim do programa")
