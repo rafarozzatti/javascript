8101de0d97ebb63f3af6907f0c35d71bf065edd9
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-15 17:11:22 -0300

    aula011 criada

---
+++ CFBCursos/aula011/aula011.js
@@ -0,0 +1,9 @@
+let v1 = 10 //Declaração de variável
+let v2 = "10" //Declaração de variável
+let v3 = v1 === v2 //Declaração de variável
+let v4 = {nome: "Rafael"} //Declaração de variável
+
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v1)) //Impressão do valor e tipo da variável
+console.log("Valor: " + v2 + " - Tipo: " + typeof(v2)) //Impressão do valor e tipo da variável
+console.log("Valor: " + v3 + " - Tipo: " + typeof(v3)) //Impressão do valor e tipo da variável
+console.log("Valor: " + v4 + " - Tipo: " + typeof(v4)) //Impressão do valor e tipo da variável
