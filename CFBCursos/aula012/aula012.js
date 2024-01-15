4a1cf3938ed3606384db1f108c1cd8d71c677921
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-15 17:34:13 -0300

    aula012 criada

---
+++ CFBCursos/aula012/aula012.js
@@ -0,0 +1,7 @@
+let n1 = [10, 20, 30] //Declaração de variável
+let n2 = [11, 22, 33, 44, 55] //Declaração de variável
+let n3 = [...n1, ...n2] //Espalhador
+
+console.log("n1: " + n1) //Impressão
+console.log("n1: " + n2) //Impressão
+console.log("n1: " + n3) //Impressão
