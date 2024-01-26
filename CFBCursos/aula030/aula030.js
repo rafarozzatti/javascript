d2065c977370845d4018a8afae9b95a10de79769
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-25 22:57:38 -0300

    aula030 criada

---
+++ CFBCursos/aula030/aula030.js
@@ -0,0 +1,13 @@
+const dc1 = document.getElementById("c1")
+const dc2 = document.getElementById("c2")
+const dc3 = document.getElementById("c3")
+const dc4 = document.getElementById("c4")
+const dc5 = document.getElementById("c5")
+const dc6 = document.getElementById("c6")
+
+const arrayElement = [dc1, dc2, dc3, dc4, dc5, dc6]
+
+arrayElement.map((e) => {
+    e.innerHTML = "Barcelona"
+    console.log(e)
+})
