e8d1b271f729c07654b4be628a62a639412ccb47
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-01-29 18:47:35 -0300

    aula038 criada

---
+++ CFBCursos/aula038/aula038.js
@@ -0,0 +1,6 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+
+console.log(caixa1.children)
+console.log(document.getRootNode())
+console.log(btn_c[0].ownerDocument)
