9182c1b041ad0aa3bc0127e7818d2b24af433c3c
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-02-21 15:45:01 -0300

    aula057 criada

---
+++ CFBCursos/aula057/aula057.js
@@ -0,0 +1,13 @@
+class Pessoa {
+    constructor(pnome) {
+        this.nome = pnome
+    }
+}
+
+let p1 = new Pessoa("Rafael")
+let p2 = new Pessoa("Adriano")
+let p3 = new Pessoa("Enzo")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
